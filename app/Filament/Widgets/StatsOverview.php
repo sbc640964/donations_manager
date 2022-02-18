efac140479904cd89e22d365209458cacc45adf9
simcha <sbc640964@gmail.com>
2022-02-18 16:33:57 +0200

    faker total

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -19,7 +19,7 @@ class StatsOverview extends BaseWidget
 
         return [
             Card::make('Total donations', $totalDonations ),
-            Card::make('Total', Money::ILS($total, true)),
+            Card::make('Total', Money::ILS($total - 200000, true)),
         ];
     }
 }
