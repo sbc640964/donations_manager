3bb46aac860e79216a0cc0b9af032a1937dd1fc5
simcha <sbc640964@gmail.com>
2022-02-19 19:36:01 +0200

    fix short full name in Contact.php

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -19,7 +19,7 @@ class StatsOverview extends BaseWidget
 
         return [
             Card::make('Total donations', $totalDonations ),
-            Card::make('Total', Money::ILS($total - 200000, true)),
+            Card::make('Total', Money::ILS($total - 300000, true)),
         ];
     }
 }
