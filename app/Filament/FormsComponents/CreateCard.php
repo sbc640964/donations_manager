58e478eec1cdd9efd65bcc51f4df6d3d68dc5df3
simcha <sbc640964@gmail.com>
2022-02-23 17:26:12 +0200

    add validate local isracard cards and password il id

--- app/Filament/FormsComponents/CreateCard.php
+++ app/Filament/FormsComponents/CreateCard.php
@@ -25,7 +25,9 @@ class CreateCard
                         return function (string $attribute, $value, Closure $fail)
                         {
                             if(!($card = CreditCard::validCreditCard($value))['valid']){
-                                return $fail("מס' הכרטיס לא תקין");
+                                if(!Validations::checkIsracard($card)){
+                                    return $fail("מס' הכרטיס לא תקין");
+                                }
                             }
                         };
                     }
@@ -55,6 +57,25 @@ class CreateCard
 
             Forms\Components\TextInput::make('password')->label('תעודת זהות')
                 ->required()
+                ->helperText('שים לב! תעודת זהות ישראלית בלבד, טפסים אחרים שמרו בצד')
+                ->rules([
+                    function () {
+                        return function (string $attribute, $value, Closure $fail) {
+                            settype($value, 'string');
+                            $sumTable = array(
+                                array(0,1,2,3,4,5,6,7,8,9),
+                                array(0,2,4,6,8,1,3,5,7,9));
+                            $sum = 0;
+                            $flip = 0;
+                            for ($i = strlen($value) - 1; $i >= 0; $i--) {
+                                $sum += $sumTable[$flip++ & 0x1][$value[$i]];
+                            }
+                            if($sum % 10 !== 0){
+                                $fail("מס' תעודת הזהות אינה תקינה");
+                            }
+                        };
+                    }
+                ])
                 ->hidden(fn(Closure $get) => !in_array($get('type'), [2,3])),
 
             Forms\Components\TextInput::make('day')->label('יום גבייה בחודש')
---
+++ app/Filament/FormsComponents/Validations.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\FormsComponents;
+
+class Validations
+{
+    static public function checkIsracard($id){
+
+        $lenId = strlen($id);
+
+        if($lenId > 9 || $lenId < 8){
+            return false;
+        }
+
+        if($lenId === 8){
+            $id = '0' . $id;
+        }
+
+        $counter = 9;
+
+        $result = 0;
+
+        foreach(str_split($id) as $digit){
+            $result += ($digit * $counter);
+            $counter --;
+        }
+
+        return $counter % 11 === 0;
+    }
+
+}
