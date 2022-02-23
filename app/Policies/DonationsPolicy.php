f41a59b7ae625e683761e13b007d168762a2cdbf
simcha <sbc640964@gmail.com>
2022-02-23 10:51:40 +0200

    update policy

--- app/Policies/DonationsPolicy.php
+++ app/Policies/DonationsPolicy.php
@@ -22,17 +22,21 @@ class DonationsPolicy
 
     public function viewAny(User $user): bool
     {
-        dd($user);
+        dd('any', $user);
         return in_array($user->id, [1,2,3,4]);
     }
 
     public function view(User $user, Donation $donation): bool
     {
+        dd('view', $user);
+
         return in_array($user->id, [1,2,3,4]);
     }
 
     public function create(User $user): bool
     {
+        dd('create', $user);
+
         return in_array($user->id, [1,3]);
     }
 
